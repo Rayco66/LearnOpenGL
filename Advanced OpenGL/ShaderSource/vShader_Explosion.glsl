211162ed404b095e9e06b86914409d90b210f9b7
Rayco66 <164200323+Rayco66@users.noreply.github.com>
2024-12-03 15:26:09 +0800

    Update vShader_Explosion.glsl
    考虑到LearnOpenGL中对于爆炸效果的错误计算(直接使用裁剪空间中的gl_Positon计算三角形图元的面法线)，我重新采用观察空间中的gl_Positon来计算面法线，并解决了问题

--- Advanced OpenGL/ShaderSource/vShader_Explosion.glsl
+++ Advanced OpenGL/ShaderSource/vShader_Explosion.glsl
@@ -18,6 +18,7 @@ void main()
 {
     vShader_out.TexCoord = aTexCoord;    
     vShader_out.FragPos = vec3(model * vec4(aPos,1.0f));
-    vShader_out.Normal = NormalMatrix * aNormal;
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    //vShader_out.Normal = NormalMatrix * aNormal;
+    vShader_out.Normal = mat3(transpose(inverse(view * model))) * aNormal;//因为我们在gShader中使用观察空间下的gl_Positon计算法线，故传给gShader的顶点法线也应该使用观察空间下的
+    gl_Position = view * model * vec4(aPos, 1.0);
 }
